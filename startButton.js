57bbebb88d722330f0ccc34e406f0c43e1e5bb33
KunhoLee <gunho1020@gmail.com>
2017-09-15 09:29:06 +0900

    Training Tracer Project
    refactoring 'index.html'
    seperate javscript out of html to 'startButton.js'

---
+++ startButton.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+    var startButton = document.getElementById('startButton');
+    startButton.addEventListener('click', function () {
+        window.open('makeTable1.html', '_self');
+    });
+}());
